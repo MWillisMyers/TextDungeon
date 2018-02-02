8804ea7250bf8a4ad21a3fbb2f03b00c1f485b04
Logan Mathis <JLsuckers@gmail.com>
2018-02-02 07:48:06 -0700

    Added base kind of protocol.  Will work on later

--- TextDungeon/player.swift
+++ TextDungeon/player.swift
@@ -11,26 +11,39 @@
  how about that!
  */
 import Foundation
-
+//base player class
+/*
 class player {
     var Health: Int
     var Attack: Double
     var Speed: Int
     var Gold: Int
     var Experience: Int
-    init() {
-        Speed = 10 //higher speed means attack first, this game is legit just a pokemon clone without the pokemon
-        Attack = 1 // this is probably going to be a multiplier on the player's attacks
-        Health = 100
-        Gold = 0 //you're broke lmao
-        Experience = 0
+    init?(Health:Int, Attack:Double, Speed:Int, Gold:Int, Experience:Int) {
+        self.Speed = Speed //higher speed means attack first, this game is legit just a pokemon clone without the pokemon
+        self.Attack = Attack // this is probably going to be a multiplier on the player's attacks
+        self.Health = Health
+        self.Gold = Gold //you're broke lmao
+        self.Experience = Experience
     }
 }
+ */
+protocol player {
+    var Health: Int {get set}
+    var Attack: Double {get set}
+    var Speed: Int {get set}
+    var Gold: Int {get set}
+    var Experience: Int {get set}
+}
 
+
+
+
+//subclasses of each of the characters
 class sorcerer: player {
-    var Mana:Int
+    var Health: Int
     var spAttack:Double
-    override init() {
+    override init(Mana:Int, spAttack:Double) {
         Mana = 100
         spAttack = 1.0 //another multiplier
     }
